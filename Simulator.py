6c783c97702ba90ffd39db3f2c818d453d57dbee
Vinicius Franceschi <vinibfranc@gmail.com>
2018-06-01 19:36:36 -0300

    Changes in parameters of simulation to maximize number of collision and availability of frames

--- Simulator.py
+++ Simulator.py
@@ -2,21 +2,23 @@
 from Transmitter import Transmitter
 import time
 import collections
-from multiprocessing import Process, Lock #not using multithreading because it doesn't provide real parallelism
+from multiprocessing import Process, Lock  # not using multithreading because it doesn't provide real parallelism
 
 '''Simulate the channel (medium) of transmission of data'''
+
+
 class Simulator:
 
     def __init__(self, lambda_number, time_slot, max_time, transmitter_count, distance_between_nodes):
 
         self.time_slot = time_slot
         self.current_time = 0
-        self.transm_time = 80
+        self.transm_time = 10
         self.max_time = max_time
         self.collision_count = 0
-        self.bandwidth = 100 #Mbps
+        self.bandwidth = 100  # Megabits por segundo
         self.speed = 2 * (10 ** 8)
-        #self.whichNodes = []
+        # self.whichNodes = []
         self.transmitter_count = transmitter_count
         self.distance_between_nodes = distance_between_nodes
         self.distance = collections.defaultdict(int)
@@ -26,13 +28,13 @@ class Simulator:
         # Assign the required number of transmitters and set up a distance between each one
         for i in range(1, self.transmitter_count + 1):
             self.transmitter.append(Transmitter(i, float(self.lambda_number) / self.time_slot))
-            self.distance[i]= (i-1) * self.distance_between_nodes
+            self.distance[i] = (i - 1) * self.distance_between_nodes
 
     # Run transmissions doing the carrier sensing and collision detection
     def run_transmissions(self):
-    # def run(self, lock):
+        # def run(self, lock):
 
-        # TODO: Made lock works
+        # TODO: Made processes lock works
         # self.create_threads()
 
         # Made possible events and detect collisions
@@ -45,7 +47,8 @@ class Simulator:
         self.collision_detection()
 
         for i in range(1, self.transmitter_count + 1):
-            print("Status do Transmissor {}: {} para {}".format(i, self.transmitter[i].status, self.transmitter[i].current_receiver))
+            print("Status do Transmissor {}: {} para {}".format(i, self.transmitter[i].status,
+                                                                self.transmitter[i].current_receiver))
         print("-------------------------------------")
         self.current_time = self.current_time + 1
 
@@ -67,38 +70,44 @@ class Simulator:
         for i in range(1, self.transmitter_count + 1):
             if self.transmitter[i].status == "Transmitindo":
                 active_transmitters.append(i)
-        if len(active_transmitters ) >= 2:
+        if len(active_transmitters) >= 2:
             self.collision_count = self.collision_count + 1
             for i in active_transmitters:
                 self.transmitter[i].stop_transmission("Colisão")
                 self.send_jam()
 
     # Notify the transmitters that a collision had occured
+    # def send_jam(self, transmitter):
     def send_jam(self):
-        #for i in range(1, self.transmitter_count + 1):
         print("Olá! Eu sou um sinal de JAM! Ocorreu uma colisão!")
-        # TODO: Stop transmitions by all stations
+        '''for i in range(1, self.transmitter_count + 1):
+            transmitter.status = "Esperando"'''
+        # TODO: Stop transmissions by all stations (change all status to Waiting)
 
     # Print basic statistics of the simulation
     def print_statistics(self):
         for i in range(1, t_count + 1):
             print("Total de quadros enviados pelo transmissor {}: {}".format(i, self.transmitter[i].frame_count - 1))
-            print("Taxa de transferência de ponta a ponta média do transmissor {}: {}".format(i, self.transmitter[i].throughput_analysis(self)))
+            print("Taxa de transferência de ponta a ponta média do transmissor {}: {}".format(i, self.transmitter[
+                i].throughput_analysis(self)))
         print("Numero de colisoes: ", self.collision_count)
         print("Tempo de simulacao: ", self.current_time)
 
+
 # Run the simulation
 if __name__ == "__main__":
-
-    # slot_time = int(input("Enter the slot time"))
-    time_sl = 50
-    frame_lambda = 0.9
+    # time_sl = 50
+    time_sl = 10
+    # frame_lambda = 0.5
+    frame_lambda = 5
     dist_transmitters = 2000
     max_time = int(input("Digite o tempo de simulação (em segundos): "))
     t_count = int(input("Digite a quantidade de transmissores: "))
+    delay = input("Ver os envio com delay? (s/n): ")
     simulation = Simulator(frame_lambda, time_sl, max_time, t_count, dist_transmitters)
     for _ in range(max_time):
-    # while True:
+        # while True:
         simulation.run_transmissions()
-        time.sleep(1)
+        if delay == "s":
+            time.sleep(1)
     simulation.print_statistics()
--- Transmitter.py
+++ Transmitter.py
@@ -2,7 +2,7 @@
 import numpy as np
 from Frame import Frame
 import random
-# import Network
+# import Simulator
 
 '''Transmitter info and methods to perform CSMA/CD control'''
 class Transmitter:
@@ -20,19 +20,17 @@ class Transmitter:
         self.current_receiver = None
 
     # Start transmission of frames
-    def start_transmission(self, station):
+    def start_transmission(self, network):
         self.status = "Transmitindo"
-        self.current_receiver = self.select_receiver(station)
-        self.current_transm_frame = ((abs(station.distance[self.id] - station.distance[self.current_receiver])) * station.distanceBetweenNodes) / station.vel
+        self.current_receiver = self.select_receiver(network)
+        self.current_transm_frame = ((abs(network.distance[self.id] - network.distance[self.current_receiver])) * network.distance_between_nodes) / network.speed
         self.frame = Frame(self.frame_count)
-        self.transm_start_time = station.current_time
-
-    #TODO: Generate real random number
+        self.transm_start_time = network.current_time
 
     # Select a receiver to a frame based on a random number and choice
-    def select_receiver(self, station):
-        random_receiver = range(1, station.frame_count)
-        #random_receiver.remove(self.id)
+    def select_receiver(self, network):
+        random_receiver = range(1, network.transmitter_count + 1)
+        # random_receiver.remove(self.id)
         return random.choice(random_receiver)
 
     # Restart transmission (used when a collision occured and the frame waited its backoff time)
@@ -49,15 +47,16 @@ class Transmitter:
         return np.random.poisson(self.lambda_number) == 1
 
     # Calculate que backoff time to all stations when a collision had occured on the medium
-    def calculate_backoff_time(self, station):
+    def calculate_backoff_time(self, network):
         self.frame.increment_collision_count()
         attempt = (2 ** self.frame.collision_count) - 1
 
         if attempt > 8:
             attempt = 8
             print("Impossível enviar, pois ocorreram muitas colisões do mesmo pacote!")
+            self.stop_transmission()
 
-        self.backoff_time = station.current_time + (np.random.randint(0, high=attempt) * station.time_slot)
+        self.backoff_time = network.current_time + (np.random.randint(0, high=attempt) * network.time_slot)
         print("Transmissor:", self.id, "ID do quadro:", self.frame.id, "Quantidade de colisões do pacote:", self.frame.collision_count, " backoff = ", self.backoff_time)
 
     # Listen the medium and verifies 4 possibilities on the transmitter
@@ -67,12 +66,12 @@ class Transmitter:
         3- If the status is Collision, we calculate the backoff and put it to wait this exponencial time
         4- If the station is waiting and its backoff ends, we try to retransmit the frame
     '''
-    def carrier_sensing(self, station):
+    def carrier_sensing(self, network):
         if self.status == 'Pronto' and self.check_if_frame_available():
-            self.start_transmission(station)
+            self.start_transmission(network)
 
         elif self.status == 'Transmitindo':
-            if self.transm_start_time + station.tt + self.current_transm_frame < station.current_time:
+            if self.transm_start_time + network.transm_time + self.current_transm_frame < network.current_time:
                 self.status = "Pronto"
                 self.transm_start_time = 0
                 self.current_transm_frame = 0
@@ -80,23 +79,23 @@ class Transmitter:
                 self.frame_count += 1
 
         elif self.status == 'Colisão':
-            self.calculate_backoff_time(station)
+            self.calculate_backoff_time(network)
             self.status = 'Esperando'
 
-        elif self.status == 'Esperando' and self.backoff_time <= station.current_time:
-            self.restart_transmission(station.current_time)
+        elif self.status == 'Esperando' and self.backoff_time <= network.current_time:
+            self.restart_transmission(network.current_time)
 
     # Here we measure the efficiency of the algorithm in the simulating condition
-    def throughput_analysis(self, station):
+    def throughput_analysis(self, network):
         pass
-        '''total_tt = (self.frame_count - 1) * station.tt
-        tp = float((station.distance[station.frame_count] - station.distance[1])
-                   * station.distanceBetweenNodes) / station.vel
-        total_collision_time = station.collCount * 2 * tp
-        total_send_time = (self.frame_count - 1) * (station.tt + tp)
+        '''total_tt = (self.frame_count - 1) * network.tt
+        #tp = float((network.distance[network.frame_count] - network.distance[1])* network.distance_between_nodes) / network.speed
+        tp = float((network.frame_count - 1) * network.distance_between_nodes * (10 ** 6)) / network.speed
+        total_collision_time = network.collCount * 2 * tp
+        total_send_time = (self.frame_count - 1) * (network.tt + tp)
         try:
             efficiency = float(total_tt)/(total_collision_time + total_send_time)
-            th = efficiency * station.bandwidth
+            th = efficiency * network.bandwidth
         except:
             th = -1
         return th'''
